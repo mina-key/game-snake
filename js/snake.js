9ceeae4bde4d49b87d1355076bf408ebc67e28fa
mina-key <minakeykhait89@gmail.com>
2023-04-19 16:29:59 +0330

    [Feat]Avoid choosing two paths at the same time

--- js/snake.js
+++ js/snake.js
@@ -2,7 +2,42 @@ import { ctx, chessSquare, canvas} from './canvas.js';
 import { drowFood} from './food.js';
 
 let snake = [{ x: 4 * chessSquare, y: 4 * chessSquare }];
+let direction;
+let dX = 32;
+let dY = 0;
+let changingDirection = false;
+
+// add event listener to change snake's direction
+document.addEventListener("keydown", changeDirection);
+function changeDirection(event) {
+  const keypress = event.keyCode;
+  if(changingDirection) return;
+  changingDirection = true;
+  switch (keypress) {
+    case 37:
+      if (direction !== "RIGHT" && snake[0].x !== chessSquare) {
+        direction = "LEFT";
+      }
+      break;
+    case 38:
+      if (direction !== "DOWN" && snake[0].y !== -chessSquare) {
+        direction = "UP";
+      }
+      break;
+    case 39:
+      if (direction !== "LEFT" && snake[0].x !== -chessSquare) {
+        direction = "RIGHT";
+      }
+      break;
+    case 40:
+      if (direction !== "UP" && snake[0].y !== chessSquare) {
+        direction = "DOWN";
+      }
+      break;
+  }
+}
 //=======================================
+
 function drowSnakePart(snakepart) {
     ctx.fillStyle = "green";
     ctx.strokeStyle = "black";
@@ -16,8 +51,25 @@ function drowSnakePart(snakepart) {
 
 //=======================================
 function moveSnake(){
-
-  let move={x:snake[0].x+chessSquare,y:snake[0].y}
+    switch (direction){
+        case "LEFT":
+          dX =-chessSquare
+          dY=0
+          break
+        case "UP" :
+          dY=-chessSquare
+          dX=0
+          break
+        case "RIGHT" :
+          dX=+chessSquare
+          dY=0
+          break
+        case "DOWN" :
+          dY=+chessSquare
+          dX=0
+          break
+      }
+  let move={x:snake[0].x+dX,y:snake[0].y+dY}
   snake.unshift(move)
    snake.pop()
 
@@ -29,9 +81,8 @@ function clearCanvas() {
   }
   //=======================================
   function firstMove() {
-  
     setTimeout(() => {
-
+        changingDirection = false;
       clearCanvas();
       drowFood();
       moveSnake();
