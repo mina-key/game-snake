61b248aa7b4d3f73810d62ab1cce25cb9501c3a7
mina-key <minakeykhait89@gmail.com>
2023-04-19 17:32:55 +0330

    [Feat]add rules gameover

--- js/snake.js
+++ js/snake.js
@@ -1,18 +1,26 @@
-import { ctx, chessSquare, canvas} from './canvas.js';
-import { drowFood} from './food.js';
-
+// import { ctx, chessSquare,canvas } from './canvas.js';
+// import { createFood, drowFood } from './food.js';
+import { ctx, chessSquare, canvas } from './canvas.js';
+import { createFood, drowFood, foodX ,foodY} from './food.js';
+//game total score
+let score = 0;
+// snake properties
 let snake = [{ x: 4 * chessSquare, y: 4 * chessSquare }];
 let direction;
 let dX = 32;
 let dY = 0;
 let changingDirection = false;
-
+//=======================================
 // add event listener to change snake's direction
 document.addEventListener("keydown", changeDirection);
+// functions
+//=======================================
+//Check keypress direction
 function changeDirection(event) {
   const keypress = event.keyCode;
   if(changingDirection) return;
   changingDirection = true;
+
   switch (keypress) {
     case 37:
       if (direction !== "RIGHT" && snake[0].x !== chessSquare) {
@@ -37,7 +45,39 @@ function changeDirection(event) {
   }
 }
 //=======================================
+function moveSnake(){
+  switch (direction){
+    case "LEFT":
+      dX =-chessSquare
+      dY=0
+      break
+    case "UP" :
+      dY=-chessSquare
+      dX=0
+      break
+    case "RIGHT" :
+      dX=+chessSquare
+      dY=0
+      break
+    case "DOWN" :
+      dY=+chessSquare
+      dX=0
+      break
+  }
+  let move={x:snake[0].x+dX,y:snake[0].y+dY}
+  snake.unshift(move)
+  if(move.x==foodX&&move.y==foodY){
+      score+=10
+document.getElementById('score').innerHTML=`Score: ${score}`
+createFood()
+  }else{
 
+      snake.pop()
+  }
+ 
+  
+}
+//=======================================
 function drowSnakePart(snakepart) {
     ctx.fillStyle = "green";
     ctx.strokeStyle = "black";
@@ -48,50 +88,52 @@ function drowSnakePart(snakepart) {
   function drowSnake() {
     snake.forEach(drowSnakePart);
   }
-
 //=======================================
-function moveSnake(){
-    switch (direction){
-        case "LEFT":
-          dX =-chessSquare
-          dY=0
-          break
-        case "UP" :
-          dY=-chessSquare
-          dX=0
-          break
-        case "RIGHT" :
-          dX=+chessSquare
-          dY=0
-          break
-        case "DOWN" :
-          dY=+chessSquare
-          dX=0
-          break
-      }
-  let move={x:snake[0].x+dX,y:snake[0].y+dY}
-  snake.unshift(move)
-   snake.pop()
-
-}
-
-//=======================================
-function clearCanvas() {
+  function clearCanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
   }
-  //=======================================
+
+
+//======================================
+//=======================================
+//game over
+function didGameEnd(){
+    //the snake hit itself
+    for(let i=1;i<snake.length;i++)
+       
+       {
+ if(snake[0].x==snake[i].x&&snake[0].y==snake[i].y)
+            return true
+       
+       } 
+       
+    
+    //hit snake to wall
+    const hitLeftWall=snake[0].x<0;
+    const hitRightWall=snake[0].x>canvas.width-chessSquare
+    const hitTopWall=snake[0].y<0
+    const hitBottomWall=snake[0].y>canvas.height-chessSquare
+    return hitLeftWall||hitRightWall||hitTopWall||hitBottomWall
+}
   function firstMove() {
+    if(didGameEnd()) {
+    
+       return
+    }
+   
     setTimeout(() => {
         changingDirection = false;
+
       clearCanvas();
       drowFood();
       moveSnake();
       drowSnake();
       firstMove();
-    }, 1000);
+    }, 500);
   }
-// draw chessboard
-   ctx.strokeRect(0, 0, canvas.width, canvas.height);
+  // draw chessboard
+  ctx.strokeRect(0, 0, canvas.width, canvas.height);
+  
 // start moving snake
-firstMove();
-export { snake};
+  firstMove();
+  export { snake};
